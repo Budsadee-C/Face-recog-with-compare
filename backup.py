e09a822cbbd16ac0808acebd7e771d167835f0d2
Budsadee Chaopatiphan <77670393+Budsadee-C@users.noreply.github.com>
2021-06-16 15:40:34 +0700

    Add files via upload

---
+++ backup.py
@@ -0,0 +1,38 @@
+import face_recognition
+import numpy as np
+import cv2
+
+person1_image = face_recognition.load_image_file("nice.jpg")
+person1_face_encoding = face_recognition.face_encodings(person1_image)[0]
+
+person2_image = face_recognition.load_image_file("nit.jpg")
+person2_face_encoding = face_recognition.face_encodings(person2_image)[0]
+
+known_face_encodings = [
+    person1_face_encoding,
+    person2_face_encoding
+]
+
+face_locations = [];
+face_encodings = [];
+face_names = [];
+process_this_frame = True;
+
+image1 = np.zeros((200,400,3), dtype='uint8');
+temp = cv2.imread("blank.png");
+temp = cv2.resize(temp,(200,200));
+image1[:200,:200] = temp;
+#print(image1.shape);
+
+while True:
+    cap = cv2.VideoCapture(0);
+    ret, frame = cap.read();
+    frame = cv2.resize(frame,(200,200), fx=0.25, fy=0.25);
+    image1[:200,200:400] = frame;
+    cv2.imshow("Show",image1);
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break;
+video_capture.release()
+cv2.destroyAllWindows()
---
+++ faceIden.py
@@ -0,0 +1,3 @@
+import cv2 #recogn for use
+import numpy as np
+from tensorflow import keras
